546cd578184758f5597a0f94c17cf534befd5e73
emmaedem <emmaontop86@yahoo.com>
2024-08-08 01:40:42 -0700

    updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,6 +10,7 @@ def sieve_of_eratosthenes(max_n):
                 is_prime[j] = False
     return is_prime
 
+
 def prime_count_up_to_n(max_n, is_prime):
     prime_count = [0] * (max_n + 1)
     count = 0
@@ -19,11 +20,12 @@ def prime_count_up_to_n(max_n, is_prime):
         prime_count[i] = count
     return prime_count
 
+
 def isWinner(x, nums):
     """This determines who is the winner after x rounds."""
     if not nums or x < 1:
         return None
-    
+
     max_n = max(nums)
     is_prime = sieve_of_eratosthenes(max_n)
     prime_count = prime_count_up_to_n(max_n, is_prime)
@@ -43,4 +45,3 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
-
